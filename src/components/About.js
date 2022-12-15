ba48776bd972e592a2310403d65fb2a38c2e6f02
+ <rodarte_sarah@yahoo.com>
2022-12-14 19:07:49 -0700

    commit tree d242c9a765f453f841295eece6bcb3c67e0a94a9

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <div id='about'>
+      <h2>About Me</h2>
+      <p>Software Engineer at Flatiron School</p>
+      <img src={image} alt="I made this"/>
+    </div>
+  </div>;
 }
 
 export default About;
